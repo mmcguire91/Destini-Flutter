6d913e4567416fc8c71b0cf240208738fe5a25aa
mmcguire91 <mmcguire91@gmail.com>
2020-05-27 14:12:58 -0500

    Update story logic

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,10 @@ class _StoryPageState extends State<StoryPage> {
                     //Choice 1 made by user.
                     //Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
                     storyBrain.nextStory(1);
+                    //Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
                   },
                   color: Colors.red,
                   child: Text(
@@ -81,6 +85,10 @@ class _StoryPageState extends State<StoryPage> {
                     //Choice 2 made by user.
                     //Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
                     storyBrain.nextStory(2);
+                    //Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+                    setState(() {
+                      storyBrain.nextStory(2);
+                    });
                   },
                   color: Colors.blue,
                   child: Text(
@@ -99,7 +107,5 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
 
 //TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
