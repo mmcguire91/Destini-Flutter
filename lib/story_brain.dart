5ab31050456d8504a5479468cc85fa45e080ba38
mmcguire91 <mmcguire91@gmail.com>
2020-05-27 14:29:59 -0500

    Code ready for debugging. Need to resolve final errors

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -39,58 +39,64 @@ class StoryBrain {
 
 // Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
   String getStory() {
-    return _storyData[storyNumber].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
     //replaced _storyData[0] with _storyData[storyNumber] after logic was defined
   }
 
 //Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
   String getChoice1() {
-    return _storyData[storyNumber].choice1;
+    return _storyData[_storyNumber].choice1;
     //replaced _storyData[0] with _storyData[storyNumber] after logic was defined
   }
 
 //Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
   String getChoice2() {
-    return _storyData[storyNumber].choice2;
+    return _storyData[_storyNumber].choice2;
     //replaced _storyData[0] with _storyData[storyNumber] after logic was defined
   }
 
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
 //Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-  int storyNumber = 0;
+//Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
+  int _storyNumber = 0;
 
   //Declare the restart variable
   void restart() {
-    storyNumber = 0;
+    _storyNumber = 0;
   }
 
 //Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
 //Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
   void nextStory(int choiceNumber) {
-    if (storyNumber == 0 && choiceNumber == 1) {
-      storyNumber = 2;
-    } else if (storyNumber == 0 && choiceNumber == 2) {
-      storyNumber = 1;
+    if (_storyNumber == 0 && choiceNumber == 1) {
+      _storyNumber = 2;
+    } else if (_storyNumber == 0 && choiceNumber == 2) {
+      _storyNumber = 1;
     }
-    if (storyNumber == 1 && choiceNumber == 1) {
-      storyNumber = 2;
-    } else if (storyNumber == 1 && choiceNumber == 2) {
-      storyNumber = 3;
+    if (_storyNumber == 1 && choiceNumber == 1) {
+      _storyNumber = 2;
+    } else if (_storyNumber == 1 && choiceNumber == 2) {
+      _storyNumber = 3;
     }
-    if (storyNumber == 2 && choiceNumber == 1) {
-      storyNumber = 5;
-    } else if (storyNumber == 2 && choiceNumber == 2) {
-      storyNumber = 4;
+    if (_storyNumber == 2 && choiceNumber == 1) {
+      _storyNumber = 5;
+    } else if (_storyNumber == 2 && choiceNumber == 2) {
+      _storyNumber = 4;
     }
     //Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-    else if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
+    else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
       restart();
     }
   }
 //Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+//Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 //Story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
